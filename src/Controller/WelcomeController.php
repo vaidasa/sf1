e91a53f57d84cd8654ca4a7a802036ed575d9dc2
Vaidas <vaidas.astrovas@distance.ktu.lt>
2018-10-01 22:10:48 +0300

    Done 4.

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,9 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController extends AbstractController
