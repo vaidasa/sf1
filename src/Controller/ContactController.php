2b79a579b84e030cc0bc2897eca21f1e81423352
Vaidas <vaidas.astrovas@distance.ktu.lt>
2018-10-02 13:13:41 +0300

    Done 6.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use phpDocumentor\Reflection\DocBlock\Description;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -11,10 +13,16 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request)
     {
      $form = $this->createForm(ContactType::class);
-     //$form = $this->createForm('App\Form\ContactType');
+
+     $form->handleRequest($request);
+
+     if ($form->isSubmitted() && $form->isValid()) {
+        $contactFormData = $form->getData();
+        dump($contactFormData);
+     }
 
      return $this->render('contact/contact.html.twig', [
        'our_form' => $form->createView(),
