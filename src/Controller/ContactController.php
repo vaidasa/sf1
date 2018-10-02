882c12e4874965fd0beb5356a547221f06aa595e
Vaidas <vaidas.astrovas@distance.ktu.lt>
2018-10-02 13:56:51 +0300

    Done 7.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -13,15 +13,26 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request)
+    public function contact(Request $request, \Swift_Mailer $mailer)
     {
      $form = $this->createForm(ContactType::class);
 
      $form->handleRequest($request);
+      $this->addFlash('info', 'Some useful info');
 
      if ($form->isSubmitted() && $form->isValid()) {
         $contactFormData = $form->getData();
         dump($contactFormData);
+
+       $message = (new \Swift_Message('You Got Mail!'))
+                       ->setFrom('test@mail.com')
+                       ->setTo('our.own.real@email.address')
+                       ->setBody($contactFormData['message'],'text/plain');
+       $mailer->send($message);
+       dump($mailer);
+       $this->addFlash('success', 'It sent!');
+
+       return $this->redirectToRoute('contact');
      }
 
      return $this->render('contact/contact.html.twig', [
